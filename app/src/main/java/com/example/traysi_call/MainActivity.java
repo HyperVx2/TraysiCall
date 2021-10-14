8f153a821f511b72d083483590ec94fd8f95e576
Chad Aga D. Abinguna <chad.abinguna@gmail.com>
2021-10-14 22:07:14 +0800

    Added default login junk. Added a button on MainActivity.java after hours of debugging.

--- app/src/main/java/com/example/traysi_call/MainActivity.java
+++ app/src/main/java/com/example/traysi_call/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.traysi_call;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.traysi_call.ui.login.login;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button splashscreen_button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        Button splashscreen_button1 = findViewById(R.id.splashscreen_button1);
+        splashscreen_button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+    public void login() {
+        Intent intent = new Intent(MainActivity.this, login.class);
+        startActivity(intent);
     }
 }
